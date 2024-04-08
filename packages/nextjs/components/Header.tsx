f7a5a284507c74dbb05712e98f4178e30539f7db
zkitty <norika@0xcerberus.io>
2024-04-08 16:59:10 -0700

    add Opensea icon

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -4,7 +4,7 @@ import React, { useCallback, useRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { Bars3Icon, FlagIcon, QuestionMarkCircleIcon, TrophyIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon, FlagIcon, GlobeAltIcon, QuestionMarkCircleIcon, TrophyIcon } from "@heroicons/react/24/outline";
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { useOutsideClick } from "~~/hooks/scaffold-eth";
 
@@ -29,6 +29,11 @@ export const menuLinks: HeaderMenuLink[] = [
     label: "Flag",
     href: "/flag",
     icon: <FlagIcon className="h-4 w-4" />,
+  },
+  {
+    label: "CyberjamNFT Collection",
+    href: "https://testnets.opensea.io/collection/cyberjam-nft",
+    icon: <GlobeAltIcon className="h-4 w-4" />,
   },
 ];
 
